d6f2c1e3b06dd57682496598271e6806c9781f03
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-03-11 09:34:23 +0300

    Update script.js
    Добавлено главное меню и вывод всех существующих заметок

--- notes/script.js
+++ notes/script.js
@@ -1,4 +1,4 @@
-// Работаем с нодой
+// Работаем с нодой (импорт)
 const readline = require("readline");
 
 //Обращаемся к фреймворку для работы с переменной
@@ -10,6 +10,8 @@ const rl = readline.createInterface({
 //Работаем с двумя видами кавычек
 const NAME_PROJ = '"NOTE"-"BOOK"';
 
+let notes = [];
+
 //Функция
 let str = `Тебя приветствует приложение ${NAME_PROJ}`;
 
@@ -19,9 +21,48 @@ const addNote = () => {
             const newNote = {
                 id: notes.length + 1,
                 title: title,
-                content: content,
+                content: context,
                 date: new Date().toLocaleString()
             };
+            notes.push(newNote);
+            console.log(`Заметка ${newNote.title} сохранена!`);
+            console.log(`Всего заметок: ${notes.length}`);
+            showMenu();
         }); 
     });
+
 }; //Стрелочная функция
+
+const showNotes = () => {
+    console.log("----Все ваши заметки----");
+    notes.forEach((note) => {
+        console.log("-".repeat(30));
+        console.log(`${note.id} * ${note.date}`);
+        console.log(`${note.title}`);
+        console.log(`${note.content}`);
+        console.log("-".repeat(30));
+    });
+};
+
+const showMenu = () => {
+    console.log(`${str}`);
+    console.log("Главное меню");
+    console.log(`Всего заметок: ${notes.length}`);
+    console.log("1.Добавить заметку");
+    console.log("2.Глянуть заметки(типо all)");
+
+    rl.question("Выберите пункт от 1 до 2:", (choice) => {
+        switch(choice){
+            case '1':
+                addNote();
+                break;
+            case '2':
+                showNotes();
+                break;
+            default:
+                console.log("плаки плаки бурмалдаки");
+        }
+    });
+};
+
+showMenu();
