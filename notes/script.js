e521cf1cdc165ae357527f8999e10c5c3c850816
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-03-12 11:35:08 +0300

    add Delete note, helper for id note

--- notes/script.js
+++ notes/script.js
@@ -1,5 +1,6 @@
 // Работаем с нодой (импорт)
 const readline = require("readline");
+const helper = require("./utils/helper");
 
 //Обращаемся к фреймворку для работы с переменной
 const rl = readline.createInterface({
@@ -13,11 +14,18 @@ const NAME_PROJ = '"NOTE"-"BOOK"';
 let notes = [];
 
 //Функция
-let str = `Тебя приветствует приложение ${NAME_PROJ}`;
+const welcome = `Тебя приветствует приложение ${NAME_PROJ}`;
+
+const welcomeApp = () => {
+    console.log('-'.repeat(42));
+    console.log(`${welcome}`);
+    console.log('-'.repeat(42));
+    showMenu();
+}
 
 const addNote = () => {
-    rl.question("Введите заголовок", (title) => {
-        rl.question("Напишите текст заметки", (context) =>{
+    rl.question("Введите заголовок: ", (title) => {
+        rl.question("Напишите текст заметки: ", (context) =>{
             const newNote = {
                 id: notes.length + 1,
                 title: title,
@@ -42,16 +50,17 @@ const showNotes = () => {
         console.log(`${note.content}`);
         console.log("-".repeat(30));
     });
+    showMenu();
 };
 
 const showMenu = () => {
-    console.log(`${str}`);
     console.log("Главное меню");
     console.log(`Всего заметок: ${notes.length}`);
     console.log("1.Добавить заметку");
     console.log("2.Глянуть заметки(типо all)");
+    console.log("3.Удалить заметку")
 
-    rl.question("Выберите пункт от 1 до 2:", (choice) => {
+    rl.question("Выберите пункт от 1 до 3:", (choice) => {
         switch(choice){
             case '1':
                 addNote();
@@ -59,10 +68,41 @@ const showMenu = () => {
             case '2':
                 showNotes();
                 break;
+            case '3':
+                deleteNote();    
             default:
                 console.log("плаки плаки бурмалдаки");
+                showMenu();
         }
     });
 };
 
-showMenu();
+const deleteNote = () => {
+    if(notes.length === 0){
+        console.log("У вас пока нет бурмалды");
+    }
+    notes.forEach((note) => {
+    console.log(`\n * [${note.id}] * ${note.title} *`);
+    });
+    rl.question(
+        "Введите номер заметки для удаления или 0 для отмены: ",
+        (choice) =>{
+        let num = parseInt(choice);
+        // if (num === 0){
+        //     // showMenu();
+        // }
+       if(num > 0 && num <= notes.length){
+        notes.splice(num -1, 1);
+        console.log(`Заметка изничтожена O_o`);
+        showMenu();
+    }   else{
+        console.log("Нет подходящей заметки")
+        showMenu();
+    }
+    showMenu();
+    },
+    );
+    showMenu();
+};
+
+welcomeApp();
---
+++ notes/utils/helper.js
@@ -0,0 +1,6 @@
+// скрипт помошник с дополнительными функциями для script.js
+const reindexId = (notes) => {
+    return notes.map((notes, index) => ({...notes, id:index +1}));
+};
+
+module.exports = {reindexId}
