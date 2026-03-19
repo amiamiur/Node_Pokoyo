bc8bd5f5dc228b68ef6fe25dbc65cb8b5d478a2a
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-03-19 12:46:27 +0300

    Create server.js

---
+++ notes/server.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+
+const helper = require("./utils/helper");
+const fileManager = require("./utils/fileManager");
+
+let notes = fileManager.loadFile();
+
+const server = http.createServer(async (req, res) => {
+  const { url, method } = req;
+
+  // ROOT ROUTERS
+
+  if(url === '/' && method === 'GET'){
+    const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+    return;
+  }
+
+  if(url === '/app.js' && method === 'GET'){
+    const js = await fs.readFile(path.join(__dirname, 'app.js'), 'utf-8');
+    res.writeHead(200, {'Content-Type': 'application/javascript'});
+    res.end(js);
+    return;
+  }
+
+  // API ROUTERS
+
+  if(url === '/api/notes' && method === 'GET'){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(notes)); 
+    return;
+  }
+
+  if(url === '/api/notes' && method === 'POST'){
+    let body = '';
+    res.on('data', chunk => body += chunk);
+    res.on('end', async() => {
+      console.log("create start");
+      const {title, content} = parseJson(body);
+      const newNote = {
+           id: notes.length + 1,
+           title: title,
+           content: content,
+           date: new Date().toLocaleString(),
+        };
+        console.log("create end");
+        notes.push(newNote);
+        fileManager.saveFile(notes);
+        console.log(`Заметка ${newNote.title} сохранена!`);
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({success : true})); 
+    });
+    return;
+  }
+
+  
+});
+
+server.listen(3000, () => {
+  console.log("Сервер запущен на порту http://localhost:3000");
+});
