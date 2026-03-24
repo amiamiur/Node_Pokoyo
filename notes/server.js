b308d6ca25efc8f118b03db2db21378d93062cb6
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-03-24 12:51:17 +0300

    Update server.js

--- notes/server.js
+++ notes/server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const fs = require('fs').promises;
-const path = require('path');
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
 
 const helper = require("./utils/helper");
 const fileManager = require("./utils/fileManager");
@@ -12,52 +12,54 @@ const server = http.createServer(async (req, res) => {
 
   // ROOT ROUTERS
 
-  if(url === '/' && method === 'GET'){
-    const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8');
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  if (url === "/" && method === "GET") {
+    const html = await fs.readFile(path.join(__dirname, "index.html"), "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
     return;
   }
 
-  if(url === '/app.js' && method === 'GET'){
-    const js = await fs.readFile(path.join(__dirname, 'app.js'), 'utf-8');
-    res.writeHead(200, {'Content-Type': 'application/javascript'});
+  if (url === "/app.js" && method === "GET") {
+    const js = await fs.readFile(path.join(__dirname, "app.js"), "utf-8");
+    res.writeHead(200, { "Content-Type": "application/javascript" });
     res.end(js);
     return;
   }
 
   // API ROUTERS
 
-  if(url === '/api/notes' && method === 'GET'){
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(notes)); 
+  if (url === "/api/notes" && method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(notes));
     return;
   }
 
-  if(url === '/api/notes' && method === 'POST'){
-    let body = '';
-    res.on('data', chunk => body += chunk);
-    res.on('end', async() => {
+  if (url === "/api/notes" && method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => (body += chunk));
+    req.on("end", async () => {
       console.log("create start");
-      const {title, content} = parseJson(body);
+      const { title, content } = JSON.parse(body);
       const newNote = {
-           id: notes.length + 1,
-           title: title,
-           content: content,
-           date: new Date().toLocaleString(),
-        };
-        console.log("create end");
-        notes.push(newNote);
-        fileManager.saveFile(notes);
-        console.log(`Заметка ${newNote.title} сохранена!`);
+        id: notes.length + 1,
+        title: title,
+        content: content,
+        date: new Date().toLocaleString(),
+      };
+      console.log("create end");
+      notes.push(newNote);
+      fileManager.saveFile(notes);
+      console.log(`Заметка ${newNote.title} сохранена!`);
 
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({success : true})); 
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ success: true }));
     });
+
+    if(url === startsWith("/api/notes") && method === 'DELETE'){
+      
+    }
     return;
   }
-
-  
 });
 
 server.listen(3000, () => {
