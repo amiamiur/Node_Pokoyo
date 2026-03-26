ba8c792e5a30eee9ef71db2a553e34d2f6332b3a
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-03-26 12:50:02 +0300

    Add DeleteNote, EditNote, ShowNotes

--- notes/app.js
+++ notes/app.js
@@ -1,5 +1,5 @@
 const stats = document.getElementById("stats");
-const content = document.getElementById("content");
+const notes_conteiner = document.getElementById("content");
 
 let notes = [];
 
@@ -8,10 +8,10 @@ async function loadNotes() {
     const res = await fetch("api/notes");
     notes = await res.json();
     if(notes.length === 0){
-        stats.innerText = "У вас отстутсвуют заметки.";
+      stats.innerText = "У вас нет заметок. Создайте свою первую заметку! \n\n";
     }
     else{
-       stats.innerText = `Заметок ${notes.length}`; 
+      stats.innerText = `Заметок ${notes.length}`;
     }
   } catch (error) {
     console.log("Ощибка", error);
@@ -22,47 +22,108 @@ async function loadNotes() {
 async function addNote() {
   const title = prompt("Введите название ");
   const content = prompt("Введите содержание ");
+  if(title === null | content === null){
+    alert("Заметка не может содержать пустое название или содержание!");
+    return;
+  }
   try {
     await fetch("api/notes", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title, content }),
     });
+    await showNotes();
   } catch (error) {
     console.log("ERROR", error.message);
   }
 }
 
 async function showNotes(){
-    loadNotes();
+    await loadNotes();
     if(notes.length === 0){
-        notes_conteiner.innerHTML = '<h2> Пока у вас нет заметок! </h2>';
+       notes_conteiner.innerHTML = '<h2> Пока у вас нет заметок! </h2>';
     }
     let html = '<h2> --- Заметки --- </h2>';
     notes.forEach((note) => {
         html += `
-        <div style = "background-color: #030202; color: #008f4a;"> 
-        <small> [ ${note.id} ] ${note.date} </small>
-        <strong> ${note.title} </strong>
-        <p> ~~~~~~~~~~~~~~~~~~~~~~~~~~ </p>
-        <strong> ${note.content} </strong>
-        </div>
+          <div style=" background-color: #000000; color: #0f7946; ">
+              <small> [ ${note.id} ] ${note.date} </small>
+              <strong> ${note.title} </strong>
+              <p> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ </p>
+              <strong> ${note.content} </strong>
+          </div>
         `;
     });
     notes_conteiner.innerHTML = html;
+    
 }
 
-async function deleteNote() {
-    loadNotes();
-    if(notes.length === 0){
-        alert("Пока нечего удалять. Создайте заметку");
-    }
+async function deleteNote(){
+  await loadNotes();
+  if(notes.length === 0){
+       alert("Пока нечего удалить! Заметок нет!");
+       return;
+  }
+  let list = notes.map(note => ` [${note.id}] ${note.title} `).join('\n');
+  const input = prompt(`Введите номер заметки для удаления: \n\n${list}`);
 
-    let list = '';
-    notes.forEach(note => {
-        list += `${note.id}: ${note.title}\n`;
+  const id_input = parseInt(input);
+  if(!id_input){
+    return;
+  }
+
+  if(id_input > 0 && id_input <= notes.length){
+    const res = await fetch(`/api/notes/${id_input}`, { method:'DELETE' });
+    if(res.ok){
+      await showNotes();
+    }
+  }
+  else{
+    alert("Отмена удаления! \n Необходимо указать номер существующей заметки!");
+  }
+  
+}
+
+async function editNote() {
+  await loadNotes();
+  if(notes.length === 0){
+       alert("Пока нечего редактировать! Создайте заметку!");
+       return;
+  }
+  let list = notes.map(note => ` [${note.id}] ${note.title} `).join('\n');
+  const input = prompt(`Введите номер заметки для изменения: \n\n${list}`);
+
+  const id_input = parseInt(input);
+  if(!id_input){
+    return;
+  }
+
+  if(id_input < 1 && id_input > notes.length){
+    alert("Такой заметки не существует!");
+    return;
+  }
+
+  const note = notes.find(note => note.id === id_input);
+  const title = prompt(`Введите название `, `${note.title}`);
+  const content = prompt("Введите содержание ", `${note.content}`);
+  if(title === null | content === null){
+    alert("Заметка не может содержать пустое название или содержание!");
+    return;
+  }
+  try {
+    await fetch(`api/notes/${id_input}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content }),
     });
-    list = list.trim(); 
+    await showNotes();
+  } catch (error) {
+    console.log("ERROR", error.message);
+  }
 }
 
 loadNotes();
+
+window.addNote = addNote;
+window.deleteNote = deleteNote;
+window.editNote = editNote;
--- notes/server.js
+++ notes/server.js
@@ -10,7 +10,6 @@ let notes = fileManager.loadFile();
 const server = http.createServer(async (req, res) => {
   const { url, method } = req;
 
-  // ROOT ROUTERS
 
   if (url === "/" && method === "GET") {
     const html = await fs.readFile(path.join(__dirname, "index.html"), "utf-8");
@@ -26,7 +25,6 @@ const server = http.createServer(async (req, res) => {
     return;
   }
 
-  // API ROUTERS
 
   if (url === "/api/notes" && method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
@@ -54,12 +52,42 @@ const server = http.createServer(async (req, res) => {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ success: true }));
     });
-
-    if(url === startsWith("/api/notes") && method === 'DELETE'){
-      
-    }
     return;
   }
+  if(url.startsWith("/api/notes/") && method === 'DELETE'){
+      const id = parseInt(url.split('/')[3]);
+      notes.splice(id - 1, 1);
+      notes = helper.reindexId(notes);
+      fileManager.saveFile(notes);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify({ success: true }));
+    }
+
+  if(url.startsWith("/api/notes/") && method === 'PUT'){
+      let body = "";
+      const id = parseInt(url.split('/')[3]);
+      req.on("data", (chunk) => (body += chunk));
+      req.on("end", async () => {
+        console.log("edit start");
+
+        const { title, content } = JSON.parse(body);
+
+        notes [id - 1] = {
+          ...notes[id - 1],
+          title: title,
+          content: content,
+          date: new Date().toLocaleString(),
+        };
+        fileManager.saveFile(notes);
+        console.log("edit end");
+        console.log(`Заметка ${title} изменена!`);
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ success: true }));
+    });
+  }
+  return;
 });
 
 server.listen(3000, () => {
